3f8a98e591e9db25084a6d2daf10ad22576a17a2
zivadinjo <zivadinjo1985@gmail.com>
2022-05-22 10:33:19 -0500

    day 3 CSS getTxt/Atribute

---
+++ src/test/java/com/cydeo/test/day_3_Css_getText_getAtribute/T1_Locators_getText.java
@@ -0,0 +1,41 @@
+package com.cydeo.test.day_3_Css_getText_getAtribute;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T1_Locators_getText {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://login1.nextbasecrm.com/");
+
+        WebElement username = driver.findElement(By.name("USER_LOGIN"));
+        username.sendKeys("incorrect");
+
+        WebElement password = driver.findElement(By.name("USER_PASSWORD"));
+        password.sendKeys("incorrect");
+
+        WebElement loginBtn = driver.findElement(By.className("login-btn"));
+        loginBtn.click();
+
+        WebElement errorMsg= driver.findElement(By.className("errortext"));
+
+        String actualText = errorMsg.getText(); // getText(); method will get text fromWebElement
+        String expectedText = "Incorrect login or password";
+
+        if (expectedText.equals(actualText)){
+            System.out.println("Incorrect login and password verification passed");
+        } else {
+            System.out.println("Incorrect login and password verification failed");
+        }
+
+        driver.quit();
+
+    }
+}
