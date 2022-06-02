1e005f336ff0b7e02cb954c0662384bb6ab70ac6
zivadinjo <zivadinjo1985@gmail.com>
2022-06-01 20:05:51 -0500

    day 6

---
+++ src/test/java/com/cydeo/test/day_6_TestNG_StaleElement/TestNGIntro.java
@@ -0,0 +1,17 @@
+package com.cydeo.test.day_6_TestNG_StaleElement;
+
+import org.testng.annotations.Test;
+
+public class TestNGIntro {
+
+    @Test
+    public void test1(){
+        System.out.println("Test1 is running ....");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("Test2 is running...");
+    }
+
+}
