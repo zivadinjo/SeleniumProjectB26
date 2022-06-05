9e09efcca8f615628b6f14bc622d04256e5adbab
zivadinjo <zivadinjo1985@gmail.com>
2022-06-05 14:56:10 -0500

    day 7

--- src/test/java/com/cydeo/test/day_7_TestNgdropdownalert_iframe/T2_State_Dropdown.java
+++ src/test/java/com/cydeo/test/day_7_TestNgdropdownalert_iframe/T2_State_Dropdown.java
@@ -3,12 +3,14 @@ package com.cydeo.test.day_7_TestNgdropdownalert_iframe;
 import com.cydeo.test.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class T2_State_Dropdown {
@@ -51,6 +53,43 @@ public class T2_State_Dropdown {
 //        Use all Select options. (visible text, value, index)
 
     }
+
+
+    @Test
+    public void multiSelectDropdowntest(){
+
+
+//        3. Select all the options from multiple select dropdown.
+
+        Select languages = new Select(driver.findElement(By.xpath("//select[@name = 'Languages']")));
+
+        System.out.println("languages.isMultiple() = " + languages.isMultiple());
+
+
+        languages.selectByValue("java");
+        languages.selectByVisibleText("C#");
+        languages.selectByIndex(3);
+
+//        4. Print out all selected values.
+//        getOptions() will return to all options in dropdown
+
+        List<WebElement> allOptions = languages.getOptions();
+        for (WebElement each : allOptions) {
+
+            each.click();
+            System.out.println("each.getText() "+each.getText());
+
+
+        }
+
+//        5. Deselect all values.
+
+        languages.deselectAll();
+
+
+    }
+
+
     @AfterMethod
     public void tearDown(){
         driver.quit();
