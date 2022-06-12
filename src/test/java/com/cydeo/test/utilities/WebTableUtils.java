7a1fd1c0dc17cdf8fa769e62cbc41a8aed15121d
zivadinjo <zivadinjo1985@gmail.com>
2022-06-12 15:51:34 -0500

    day 11

---
+++ src/test/java/com/cydeo/test/utilities/WebTableUtils.java
@@ -0,0 +1,15 @@
+package com.cydeo.test.utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class WebTableUtils {
+    public static String returnOrderDate(WebDriver driver, String customerName){
+
+        String locator ="//td[.='" + customerName + "']//following-sibling::td[3]";
+        WebElement customerDateCell = driver.findElement(By.xpath(locator));
+
+        return customerDateCell.getText();
+    }
+}
