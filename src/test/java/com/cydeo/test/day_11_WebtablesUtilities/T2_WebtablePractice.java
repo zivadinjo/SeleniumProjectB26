7c4362bb923b52e6df64f8015e7134fc30c71203
zivadinjo <zivadinjo1985@gmail.com>
2022-06-14 19:28:08 -0500

    day 11

--- src/test/java/com/cydeo/test/day_11_WebtablesUtilities/T2_WebtablePractice.java
+++ src/test/java/com/cydeo/test/day_11_WebtablesUtilities/T2_WebtablePractice.java
@@ -1,6 +1,7 @@
 package com.cydeo.test.day_11_WebtablesUtilities;
 
 import com.cydeo.test.base.TestBase;
+import com.cydeo.test.utilities.WebTableUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
@@ -33,6 +34,26 @@ public class T2_WebtablePractice extends TestBase {
         String expectedDate = "12/31/2021";
 
         Assert.assertEquals(actualDate,expectedDate,"Date verification failed");
+
+    }
+
+    @Test
+    public void test2(){
+        driver.get("https://practice.cydeo.com/web-tables");
+
+        String customerOrderDate1 = WebTableUtils.returnOrderDate(driver,"Alexandra Gray");
+        System.out.println("customerOrderDate1 = " + customerOrderDate1);
+
+        String customerOrderDate2 = WebTableUtils.returnOrderDate(driver,"Ned Stark");
+        System.out.println("customerOrderDate2 = " + customerOrderDate2);
+
+    }
+
+    @Test
+    public void test3(){
+        driver.get("https://practice.cydeo.com/web-tables");
+
+        WebTableUtils.orderVerify(driver,"Alexandra Gray","04/15/2021");
 
     }
 
--- src/test/java/com/cydeo/test/utilities/WebTableUtils.java
+++ src/test/java/com/cydeo/test/utilities/WebTableUtils.java
@@ -3,6 +3,7 @@ package com.cydeo.test.utilities;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 
 public class WebTableUtils {
     public static String returnOrderDate(WebDriver driver, String customerName){
@@ -12,4 +13,20 @@ public class WebTableUtils {
 
         return customerDateCell.getText();
     }
+
+//    public static String returnOrderDate2(WebDriver driver,String customerName){
+//        return driver.findElement(By.xpath("//td[.='" + customerName + "']//following-sibling::td[3]")).getText();
+//    }
+
+    public static void orderVerify(WebDriver driver,String customerName,String expectedOrderDate){
+
+        String locator ="//td[.='" + customerName + "']//following-sibling::td[3]";
+
+        WebElement customerDateCell = driver.findElement(By.xpath(locator));
+
+        Assert.assertEquals(customerDateCell.getText(),expectedOrderDate);
+
+    }
+
+
 }
