98293081cbce2ac9335c8ded4b33d4d71116afbb
zivadinjo <zivadinjo1985@gmail.com>
2022-06-14 20:34:13 -0500

    day 12

--- src/test/java/com/cydeo/test/day_12_learningProperties/learningProperties.java
+++ src/test/java/com/cydeo/test/day_12_learningProperties/learningProperties.java
@@ -6,7 +6,10 @@ public class learningProperties {
 
     @Test
     public void java_properties_reading_test(){
-
+//      key     =   value
+//      os.mame =   Windows 10
+        System.out.println("System.getProperty(\"os.name\") = " + System.getProperty("os.name"));
+        System.out.println("System.getProperty(\"user.name\") = " + System.getProperty("user.name"));
     }
 
 }
