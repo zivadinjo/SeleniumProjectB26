e3d04bf0eda77ba5eca3095f91e36f636895136d
zivadinjo <zivadinjo1985@gmail.com>
2022-06-18 10:32:53 -0500

    day 14

--- src/test/java/com/cydeo/test/utilities/Driver.java
+++ src/test/java/com/cydeo/test/utilities/Driver.java
@@ -1,6 +1,11 @@
 package com.cydeo.test.utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
 
 public class Driver {
 
@@ -12,7 +17,7 @@ public class Driver {
 
 //    We make WebDriver private,because we want to close access from outside class
 //    We make it static,because we will use it in static method
-    private static WebDriver driver;
+    private static WebDriver driver; // values is null by default
 
     public static WebDriver getDriver(){
 
@@ -20,6 +25,23 @@ public class Driver {
 //         if you already setup driver and using it again for following line of codes, it will return to same driver
 
         if(driver == null){
+//            We read browserType from configuration.properties with help of ConfigurationReader class and getProperty() method
+            String browserType = ConfigurationReader.getProperty("browser");
+
+            switch (browserType){
+                case "chrome":
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+            }
 
         }
         return driver;
