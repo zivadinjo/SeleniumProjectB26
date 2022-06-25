52a222cddd10e503be14ae365873efc69379910d
zivadinjo <zivadinjo1985@gmail.com>
2022-06-25 11:59:00 -0500

    day 17

--- src/test/java/com/cydeo/test/day_17_pom_syncronization/T2_POM_ActionsPractice.java
+++ src/test/java/com/cydeo/test/day_17_pom_syncronization/T2_POM_ActionsPractice.java
@@ -3,6 +3,7 @@ package com.cydeo.test.day_17_pom_syncronization;
 import com.cydeo.pages.CydeoPracticePage;
 import com.cydeo.test.utilities.Driver;
 import com.google.common.base.Verify;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -20,22 +21,29 @@ public class T2_POM_ActionsPractice {
 //    3. Verify expected default text appears on big circle
 //    Expected = "Drag the small circle here."
 
-        Assert.assertEquals(cydeoPracticePage.bigCircle.getText(),"Drag the small circle here.");
+        Assert.assertEquals(cydeoPracticePage.bigCircle.getText(), "Drag the small circle here.");
 
     }
 
     @Test
-    public void drop_text_here(){
+    public void drop_text_here() {
 
 //    TC #3: Cydeo Practice Drag and Drop Test
 //    1. Open a chrome browser
 //    2.  Go to:https://practice.cydeo.com/drag_and_drop_circles
+        Driver.getDriver().get("https://practice.cydeo.com/drag_and_drop_circles");
 
+        CydeoPracticePage cydeoPracticePage = new CydeoPracticePage();
 
 //     3. Click and hold small circle
-
+        Actions actions = new Actions(Driver.getDriver());
+        actions.moveToElement(cydeoPracticePage.smallCircle).clickAndHold().moveByOffset(100,50).perform(); // finding,clicking and holding small circle,moveByOffset moves circle by X and Y arrows, and it allows to show up drop here in big circle
 
 //     4. Verify "Drop here." text appears on big circle
+
+        Assert.assertEquals(cydeoPracticePage.bigCircle.getText(),"Drop here.");
+
+        Driver.closeDriver();
 
     }
 
