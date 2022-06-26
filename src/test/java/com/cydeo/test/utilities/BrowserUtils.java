82b5c3a34316250f8bb1bee6a5f21362803c13bb
zivadinjo <zivadinjo1985@gmail.com>
2022-06-26 11:31:37 -0500

    day 18

--- src/test/java/com/cydeo/test/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/test/utilities/BrowserUtils.java
@@ -1,6 +1,9 @@
 package com.cydeo.test.utilities;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.util.Set;
@@ -45,4 +48,10 @@ public class BrowserUtils {
         Assert.assertEquals(driver.getTitle(),expectedTitle);
 
     }
+
+    public static void waitForInvisibilityOf(WebElement element){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
+        wait.until(ExpectedConditions.invisibilityOf(element));
+    }
+
 }
